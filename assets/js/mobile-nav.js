161b48fefac0eccebcc0aeb47305049d2b878f10
KallunaLovegeed <heather@balderdash.co>
2015-03-02 15:57:27 -0600

    added transition delays to home and our mattress

---
+++ assets/js/mobile-nav.js
@@ -0,0 +1,9 @@
+jQuery(document).ready(function($){
+  
+
+  $("#hamburger-container").on("click", function(){
+    $("#hamburger-button").toggleClass("expanded");
+    $("#mobile-nav").toggleClass("expanded");
+  });
+
+});
