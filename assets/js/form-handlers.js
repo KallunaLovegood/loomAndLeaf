d3b33b344b0f8a67539089933f765992171db06b
KallunaLovegeed <heather@balderdash.co>
2015-03-03 14:11:22 -0600

    created form handler for request brochure

--- assets/js/form-handlers.js
+++ assets/js/form-handlers.js
@@ -1,8 +1,9 @@
 // FORM HANDLERS
 $(function () {
 
-  function submitForm(e) {
+  function submitContactForm(e) {
     //prevents page refresh (point of AJAX)
+    // alert ("works");
     e.preventDefault();
 
     // get the data from the form
@@ -12,6 +13,7 @@ $(function () {
       contact_emailphone: $('#contact_emailphone').val(),
       contact_msg: $('#contact_msg').val(),
     };
+    // console.log (payload) - make sure you're capturing everything
 
     // send off request to server (this is where we make an AJAX request)
     $.ajax({
@@ -19,17 +21,59 @@ $(function () {
       url: 'http://ll_dev.saatvamattress.com/ajax-contact.php',
       data: payload,
       success: function (data, status) {
-        console.log('SUCCES', data, status);
+        console.log('SUCCESS', data, status);
         // FLASH MESSAGE: awesome!
+        alert ("woohoo!");
       },
       error: function (jqXHR, status) {
         console.log('ERROR', status);
         // FLASH MESSAGE: WOMP WOMP
+        alert ("nope!");
+        return false;
       }
     });
-
   }
 
-  // handler to submit the form.
-  $('.questionForm-button').click(submitForm);
+  function submitBrochureForm(e) {
+    // prevents page refresh
+    // alert ("works!");
+    e.preventDefault();
+
+    // get the data from the form
+    var payload = {
+      firstname: $('#firstname').val(),
+      first_name: $('#first_name').val(),
+      last_name: $('#last_name').val(),
+      address: $('#address').val(),
+      address2: $('#address2').val(),
+      city: $('#city').val(),
+      state: $('#state').val(),
+      zip: $('#zip').val(),
+      email: $('#email').val(),
+    };
+    // console.log(payload); - make sure everything is captured
+
+    // send off request to server
+    $.ajax({
+      type: 'POST',
+      url: 'http://ll_dev.saatvamattress.com/ajax-brochure.php',
+      data: payload,
+      success: function (data, status) {
+        console.log('SUCCESS', data, status);
+        // flash message: awesome
+        alert ("woohoo!");
+      },
+      error: function (jqXHR, status) {
+        console.log('ERROR', status);
+        // flash message: nope
+        // return false;
+        alert ("nope");
+      }
+    });
+  }
+
+  // contact form handler to submit the form.
+  $('.questionForm-button').click(submitContactForm);
+  // brochure form handler to submit the form
+  $('.brochureForm-button').click(submitBrochureForm);
 });
