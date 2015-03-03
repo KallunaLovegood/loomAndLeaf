eb511973a1766a21cec8c698838d73c0dddc70f0
KallunaLovegeed <heather@balderdash.co>
2015-03-03 15:17:48 -0600

    added hover effects to comfort levels page

---
+++ assets/js/fun-effects.js
@@ -0,0 +1,21 @@
+// FUN EFFECTS
+jQuery(document).ready(function($){
+
+// HOVER FOR COMFORT LEVELS PAGE
+$( ".relaxed-firm" ).hover(
+  function() {
+    $(".firmscale-container").removeClass("non-hover");
+    $("#relaxedfirm-snippet").show();
+    $("#firm-snippet").hide();
+  });
+
+$(".firm").hover(
+  function() {
+    $(".firmscale-container").removeClass("non-hover");
+    $("#relaxedfirm-snippet").hide();
+    $("#firm-snippet").show();
+  });
+
+
+
+});
--- assets/js/mobile-nav.js
+++ assets/js/mobile-nav.js
@@ -6,4 +6,6 @@ jQuery(document).ready(function($){
     $("#mobile-nav").toggleClass("expanded");
   });
 
+
+
 });
